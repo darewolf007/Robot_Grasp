3005729e3f1e53908159bd89c89743732834a3d9
darewolf007 <1599895936@qq.com>
2023-11-06 16:55:40 +0800

    test

--- Robot_Control/test/test_sawyer.py
+++ Robot_Control/test/test_sawyer.py
@@ -9,6 +9,6 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
-
+   
 if __name__ == '__main__':
     unittest.main()
